34662655f2cae5bc19fd6db59e61e2c18c2f3a20
neeru0713 <neerurani1307@gmail.com>
2023-07-02 17:46:42 +0530

    table header alignment

--- src/components/Table.js
+++ src/components/Table.js
@@ -5,7 +5,7 @@ const Table = ({ data }) => {
   return (
     <div className="h-[70%] w-[100%] bg-[#1d2833] border border-0 rounded-lg mt-20">
       <table className="flex flex-col font-semibold text-gray-400 text-sm pt-8 ml-10 w-[100%]">
-        <thead className="flex justify-between font-semibold text-gray-400 text-sm pt-8 w-[90%]">
+        <thead className="flex justify-between font-semibold text-gray-400 text-sm pt-8 w-[90%] mb-20">
           <th>Collateral Asset</th>
           <th>Total Supply</th>
           <th>Oracle Price</th>
